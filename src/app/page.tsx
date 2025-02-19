0feeb63fa1ad2370cbd5b760729a8bc4490e8488
thrain-traitor <arkadanisaysstuff@gmail.com>
2025-02-19 21:51:13 +0700

    plus

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Hero from "@/components/hero"
 import Footer from "@/components/footer"
+import Contribution from "@/components/contribution";
 
 export default function Home() {
   return (
     <>
     <div className="h-full">
       <Hero/>
-      
+      <Contribution/>
       <Footer/>
     </div>
     </>
---
+++ src/components/contribution.tsx
@@ -0,0 +1,17 @@
+export default function contribution() {
+  return(
+    <>
+    <div className="border-b-1 border-zinc-800"/>
+    <div className="w-full px-10 lg:px-20">
+      <div className="flex justify-between bg-zinc-950 border-x border-x-zinc-800 px-2">
+        <p className="text-zinc-800 text-xs lg:text-lg font-light">anjay</p>
+        <p className="text-zinc-800 text-xs lg:text-lg font-light">Contribution Comp</p>
+      </div>
+    </div>
+    <div className="border-b-1 border-zinc-800 w-full"/>
+    <div>
+      bangsat
+    </div>
+    </>
+  )
+}
