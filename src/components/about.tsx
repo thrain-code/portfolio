733fb85f81342acfeffaa24372c1397092fc6b8c
thrain-traitor <arkadanisaysstuff@gmail.com>
2025-02-24 01:02:40 +0700

    yeah

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -29,18 +29,14 @@ const Timeline = () => {
   ];
 
   return (
-    <div className="relative flex flex-col space-y-6">
-
-      {/* Render Timeline Items */}
+    <div className="space-y-2 flex flex-col">
       {timelineData.map((item, index) => (
-        <div key={index} className="relative flex items-start space-x-4 group">
-          <div className="w-10 h-10 flex items-center justify-center">
+        <div key={index} className="flex flex-col">
+          <div className="flex px-3 text-xl font-semibold">
             {item.step}
+            <p className="text-zinc-600 px-2 pb-2 text-sm lg:text-lg font-light">{item.description}</p>
           </div>
-          <p className="text-zinc-300 text-lg pb-3 rounded-xl">
-            {item.description}
-            <div className="border-b mt-3 border-zinc-800 w-full" />
-          </p>
+          <div className="border-b border-zinc-800 w-full" />
         </div>
       ))}
     </div>
@@ -75,30 +71,32 @@ function AboutStory() {
         </button>
       </div>
 
-      <div className="text-zinc-300 lg:text-lg text-sm px-4">
-        {activeTab === "journey" && (
-          <Timeline/>
-        )}
+      <div className="text-zinc-300 lg:text-lg text-sm">
+        {activeTab === "journey" && <Timeline />}
         {activeTab === "mindset" && (
-          <p>
-            As a developer, I have never believed in talent. Talent is just luck
-            granted to those who don’t even have to try. Meanwhile, I have to
-            struggle, sacrifice my time, energy, and mind just to reach the same
-            point as them. I hate seeing people who can easily master something
-            without having to go through a long and painful process. I hate
-            seeing them sitting comfortably while I have to fight through every
-            line of code, every error that crushes my mental state, and every
-            failure that feels like a hard blow to the head. But that hatred is
-            not a weakness—it is fuel. I let it drive me, burn me from within,
-            and force me to move forward. If I don’t have talent, then I will
-            replace it with unmatched hard work. If I don’t have a natural
-            advantage, then I will carve it out with blood and sweat. I will
-            learn, try, fail, and rise again until I can stand at the top and
-            look down—at those who only rely on luck without effort. I will not
-            accept defeat, and I will not let those who are lazy and complacent
-            surpass me. If the world is unfair, then I will force justice with
-            my own hands.
-          </p>
+          <div>
+            <h1 className="px-2 pb-2 text-xl font-semibold">Always never satisfied with your work to push you forward...</h1>
+            <div className="border-b border-zinc-800 w-full" />
+            <p className="text-zinc-600 px-2 pb-2 text-sm lg:text-lg">
+              As a developer, I have never believed in talent. Talent is just
+              luck granted to those who don’t even have to try. Meanwhile, I
+              have to struggle, sacrifice my time, energy, and mind just to
+              reach the same point as them. I hate seeing people who can easily
+              master something without having to go through a long and painful
+              process. I hate seeing them sitting comfortably while I have to
+              fight through every line of code, every error that crushes my
+              mental state, and every failure that feels like a hard blow to the
+              head. But that hatred is not a weakness—it is fuel. I let it drive
+              me, burn me from within, and force me to move forward. If I don’t
+              have talent, then I will replace it with unmatched hard work. If I
+              don’t have a natural advantage, then I will carve it out with
+              blood and sweat. I will learn, try, fail, and rise again until I
+              can stand at the top and look down—at those who only rely on luck
+              without effort. I will not accept defeat, and I will not let those
+              who are lazy and complacent surpass me. If the world is unfair,
+              then I will force justice with my own hands.
+            </p>
+          </div>
         )}
       </div>
     </div>
@@ -119,13 +117,12 @@ export default function AboutComp() {
           </div>
           <div className="h-px lg:h-full bg-zinc-800 w-full lg:w-px lg:my-0 hidden lg:block" />
           <div className="h-full w-full items-center flex flex-col justify-center">
-
             <div
               className={`lg:flex-col flex mx-4 z-40 transition-all py-5 rotate-x-50 rotate-z-45 ${
                 isTyping ? "lg:mb-10 mb-7" : ""
               }`}
             >
-              <div className="border-1 bg-zinc-950 p-1 w-48 h-48 lg:w-92 lg:h-96 border-zinc-400 rounded-xl">
+              <div className="border-1 bg-zinc-950 p-1 w-48 h-48 lg:w-92 lg:h-96 relative border-zinc-400 rounded-xl">
                 <div className="flex items-center lg:space-x-2 space-x-1 px-1 lg:py-2 py-1 rounded-t-lg">
                   <span className="lg:h-5 lg:w-5 h-2 w-2 border-1 border-zinc-400 rounded-full"></span>
                   <span className="lg:h-5 lg:w-5 h-2 w-2 border-1 border-zinc-400 rounded-full"></span>
@@ -133,7 +130,7 @@ export default function AboutComp() {
                 </div>
                 <div className="border-1 space-y-2 lg:h-84 h-41 rounded-lg border-zinc-400 p-3">
                   <div className="flex gap-3">
-                    <FaUser className="h-6" />
+                    <FaUser className="lg:h-6 h-3" />
                     <div className="border rounded-xl w-full border-zinc-400" />
                   </div>
                   <div className="border-1 flex border-zinc-400 h-28 lg:h-70 rounded-xl">
@@ -145,7 +142,7 @@ export default function AboutComp() {
                         onFocus={() => setIsTyping(true)}
                         onBlur={() => setIsTyping(false)}
                       />
-                      <button className="border lg:text-md text-xs font-semibold border-zinc-400 rounded-xl mx-3 mb-3 py-1 text-zinc-300 transition-all duration-300 ease-in-out hover:bg-zinc-400 hover:text-black active:scale-95">
+                      <button className="border lg:text-xl text-xs font-semibold border-zinc-400 rounded-xl mx-3 mb-3 py-1 text-zinc-300 transition-all duration-300 ease-in-out hover:bg-zinc-400 hover:text-black active:scale-95">
                         Submit
                       </button>
                     </div>
@@ -155,7 +152,7 @@ export default function AboutComp() {
             </div>
 
             <div className="lg:flex-col flex mx-4 z-30 absolute rotate-x-50 rotate-z-45 shadow-xl shadow-zinc-900 rounded-lg">
-            <div className="border-1 bg-zinc-950 p-1 w-48 h-48 lg:w-92 lg:h-96 border-zinc-400 rounded-xl">
+              <div className="border-1 bg-zinc-950 p-1 w-48 h-48 lg:w-92 lg:h-96 border-zinc-400 rounded-xl">
                 <div className="flex items-center lg:space-x-2 space-x-1 px-1 lg:py-2 py-1 rounded-t-lg">
                   <span className="lg:h-5 lg:w-5 h-2 w-2 border-1 border-zinc-400 rounded-full"></span>
                   <span className="lg:h-5 lg:w-5 h-2 w-2 border-1 border-zinc-400 rounded-full"></span>
