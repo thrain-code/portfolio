470e6cc40085c5622ac7eae2ffbbaecaf6b987d5
thrain-traitor <arkadanisaysstuff@gmail.com>
2025-03-05 01:02:00 +0700

    add motion

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,17 @@
+"use client";
+
+import Link from "next/link";
+
 export default function NotFound() {
   return (
-    <div className="px-10 lg:px-20">
-    <div className="flex flex-col items-center justify-center min-h-screen bg-black">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white">
       <h1 className="text-6xl font-bold">404</h1>
-      <p className="text-xl mt-4">Halaman gaa ada tolol</p>
-      <a href="/" className="mt-6 px-4 py-2 bg-white text-black rounded-md hover:bg-gray-300 transition">
-        Kembali ke Beranda
-      </a>
-    </div>
+      <p className="text-xl mt-4">Halaman tidak ditemukan</p>
+      <Link href="/">
+        <span className="mt-6 px-4 py-2 bg-white text-black rounded-md hover:bg-gray-300 transition cursor-pointer">
+          Kembali ke Beranda
+        </span>
+      </Link>
     </div>
   );
 }
-NotFound.getLayout = (page: React.ReactNode) => <>{page}</>;
